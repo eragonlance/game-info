89f0f178901aca527d394d259c7e80707b23a87d
eragonlance <world.apocalypse@hotmail.com>
2018-08-14 14:04:48 +0700

    Add dummy index page

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from gameinfo import views
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+urlpatterns = [path("", views.index, name="index"), path("admin/", admin.site.urls)]
