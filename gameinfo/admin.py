c0291f03645ac657bfd995716d90a04c4260cbfd
eragonlance <world.apocalypse@hotmail.com>
2018-10-04 17:28:57 +0700

    Register models for admin

--- gameinfo/admin.py
+++ gameinfo/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+import gameinfo.models as models
+
 # Register your models here.
+admin.site.register(models.Platform)
+admin.site.register(models.Developer)
+admin.site.register(models.Publisher)
+admin.site.register(models.Genre)
+admin.site.register(models.Game)
+admin.site.register(models.Release)
+admin.site.register(models.Url)
